042f591510cec58226b6e300b45c0596426675aa
hoangperry <prodai9573@gmail.com>
2019-11-08 18:21:27 +0700

    init

---
+++ run.py
@@ -0,0 +1,10 @@
+class Robot:
+    def __init__(self, height: int, width: int):
+        self.broad = [[0] * width] * height
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    robot = Robot()
